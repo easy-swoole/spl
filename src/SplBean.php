f02055a227f75ce639ae7ab40e265acf2cde1f2c
leocavalcante <lc@leocavalcante.com>
2020-11-30 15:56:04 -0300

    fix: private methods cannot be final

--- src/SplBean.php
+++ src/SplBean.php
@@ -108,7 +108,7 @@ class SplBean implements \JsonSerializable
         return $array;
     }
 
-    final private function arrayToBean(array $data, $autoCreateProperty = false): SplBean
+    private function arrayToBean(array $data, $autoCreateProperty = false): SplBean
     {
 
         $data = $this->dataKeyMap($data);
@@ -251,7 +251,7 @@ class SplBean implements \JsonSerializable
      * @param array $array
      * @return array
      */
-    final private function beanKeyMap(array $array): array
+    private function beanKeyMap(array $array): array
     {
         foreach ($this->setKeyMapping() as $dataKey => $beanKey) {
             if (array_key_exists($beanKey, $array)) {
@@ -269,7 +269,7 @@ class SplBean implements \JsonSerializable
      * @param array $array
      * @return array
      */
-    final private function dataKeyMap(array $array): array
+    private function dataKeyMap(array $array): array
     {
         foreach ($this->setKeyMapping() as $dataKey => $beanKey) {
             if (array_key_exists($dataKey, $array)) {
